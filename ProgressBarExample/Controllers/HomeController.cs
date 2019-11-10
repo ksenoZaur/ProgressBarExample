38d43daea43e0521cda5190ebb2da4a88e5aafbf
ksenoZaur <playboyfrombulanikha@gmail.com>
2019-11-10 13:02:53 +0400

    Обновление прогрессбара, остановка отслеживания по завершению задачи.

--- ProgressBarExample/Controllers/HomeController.cs
+++ ProgressBarExample/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json.Linq;
 using ProgressBarExample.Models;
 
 namespace ProgressBarExample.Controllers
@@ -29,48 +31,59 @@ namespace ProgressBarExample.Controllers
             return View();
         }
 
-        private async Task ImportFile()
+        [HttpPost]
+        public async Task ImportFile()
         {
-            ProgressBarData pgData = new ProgressBarData();
-            pgData.IsComplete = false;
-            pgData.Percentage = 0;
-            pgData.Status = "Starting";
-            pgData.Total = 0;
+            ProgressBarData pgData = new ProgressBarData()
+            {
+                IsComplete = false,
+                Percentage = 0,
+                Status = "Starting",
+                Total = 0,
+            };
+            
             string strToSave;
             int size = 10;
             
+            // Вычислительная операция, которая занимает много времени
             await Task.Run( ()=>{
                 for (int i = 0; i < size; i++)
                 {
                     strToSave = Newtonsoft.Json.JsonConvert.SerializeObject(pgData);
-                    Console.WriteLine(strToSave);
+                    // Сохранить строку в общую область
+                    HttpContext.Cache.Insert("Progress", strToSave);
+                    
                     pgData.Total++;
                     pgData.Percentage = (int)((double) pgData.Total / (double) size * 100);
                     pgData.Status = "Processing";
-                    // Сохранить строку в общую область
+                    // Имитация длительного процесса
                     Thread.Sleep(1000);
                 }
+                
                 pgData.Percentage = (pgData.Total * 100) / pgData.Total;
                 pgData.Status = "Completed";
                 pgData.IsComplete = true;                
                 
-                strToSave = Newtonsoft.Json.JsonConvert.SerializeObject(pgData);
-                Console.WriteLine(strToSave);
                 // Сохранить строку в общую область
-            });
+                strToSave = Newtonsoft.Json.JsonConvert.SerializeObject(pgData);
+                HttpContext.Cache.Insert("Progress", strToSave);
+             });
         }
         
         [HttpPost]
         public string GetProgress()
-        {    
+        {
+            string progress = null;
             // Получить текущий прогресс из общей области
-            return Newtonsoft.Json.JsonConvert.SerializeObject(new ProgressBarData()
+            try
             {
-                Percentage = 25,
-                Status = "Processing",
-                Total = 5,
-                IsComplete = false
-            });
+                progress = HttpContext.Cache.Get("Progress").ToString(); 
+            }
+            catch (Exception e)
+            {
+                progress = "Не удалось получить информацию о прогрессе!!!";
+            }
+            return progress;
         }
     }
 }
